8923787612ad8b1d6e0bc0ed546b097fc40d230f
Krishna Sai Cholkar <krishnasai3cks@gmail.com>
2021-01-08 01:37:15 +0530

    new changes

---
+++ projects.js
@@ -0,0 +1,218 @@
+let list = [{
+        technology: "Responsive Web Design",
+        projects: [{
+                prolink: "https://codepen.io/tznqeyiq/full/wvMxPOb",
+                paragraph: "Tribute page",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/full/wvMxpjM",
+                paragraph: "Form",
+            },
+
+            {
+                prolink: "https://codepen.io/tznqeyiq/full/XWXBEVd",
+                paragraph: "Product Landing",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/full/rNxZQeE",
+                paragraph: "Technical Documentation",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/full/LYGJqEM",
+                paragraph: "Personal portfolio",
+            },
+        ],
+    },
+    {
+        technology: "Front End Libraries",
+        projects: [{
+                prolink: "https://codepen.io/tznqeyiq/full/bGEJQvN",
+                paragraph: "Random Quote Machine",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/pen/oNbRWOW?editors=1111",
+                paragraph: "Markdown Previewer",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/pen/LYGojBK?editors=1111",
+                paragraph: "Drum Machine",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/pen/OJMYYOO?editors=0010",
+                paragraph: "Calculator",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/pen/qBbzpLg?editors=0010",
+                paragraph: "Pomodoro Clock",
+            },
+        ],
+    },
+    {
+        technology: "Data Visualization",
+        projects: [{
+                prolink: "https://codepen.io/tznqeyiq/pen/VweoxzG?editors=0011",
+                paragraph: "Bar Chart",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/pen/eYJqwYX?editors=1111",
+                paragraph: "Scatterplot Graph",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/full/gOrYZVN",
+                paragraph: "Heat Map",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/full/oNxvrRB",
+                paragraph: "Choropleth Map",
+            },
+            {
+                prolink: "https://codepen.io/tznqeyiq/full/dyMyVxx",
+                paragraph: "Treemap",
+            },
+        ],
+    },
+    {
+        technology: "APIs and Microservices",
+        projects: [{
+                prolink: "https://timestamp-microservice.krishnasai4.repl.co/",
+                paragraph: "Timestamp Microservice",
+            },
+            {
+                prolink: "https://header-parser.krishnasai4.repl.co/",
+                paragraph: "Request Header parser",
+            },
+            {
+                prolink: "https://spiritual-sunrise-okra.glitch.me/",
+                paragraph: "Url Shortener",
+            },
+            {
+                prolink: "https://exercisetracker.krishnasai4.repl.co/",
+                paragraph: "Exercise Tracker",
+            },
+            {
+                prolink: "https://filemetadata.krishnasai4.repl.co/",
+                paragraph: "File Metadata",
+            },
+        ],
+    },
+    {
+        technology: "Quality Assurance",
+        projects: [{
+                prolink: "https://rogue-plume-bug.glitch.me/",
+                paragraph: "Metric/Imperial",
+            },
+            {
+                prolink: "https://rhetorical-hurricane-rugby.glitch.me/",
+                paragraph: "Issue Tracker",
+            },
+            {
+                prolink: "https://hilarious-aromatic-sovereign.glitch.me/",
+                paragraph: "Personal Library",
+            },
+            {
+                prolink: "https://indecisive-lofty-network.glitch.me/",
+                paragraph: "Sudoku Solver",
+            },
+            {
+                prolink: "https://cuboid-working-onyx.glitch.me/",
+                paragraph: "American British Translator",
+            },
+        ],
+    },
+    {
+        technology: "Scientific Computing with Python",
+        projects: [{
+                prolink: "https://growingneglectedstrategies.krishnasai4.repl.run/",
+                paragraph: "Arithmetic Formatter",
+            },
+            {
+                prolink: "https://fcc-time-calculator.krishnasai4.repl.run/",
+                paragraph: "Time Calculator",
+            },
+            {
+                prolink: "https://fcc-budget-app.krishnasai4.repl.run/",
+                paragraph: "Budget App",
+            },
+            {
+                prolink: "https://fcc-shape-calculator.krishnasai4.repl.run/",
+                paragraph: "Ploygon Area Calculator",
+            },
+            {
+                prolink: "https://fcc-probability-calculator.krishnasai4.repl.run/",
+                paragraph: "Probability Calculator",
+            },
+        ],
+    },
+    {
+        technology: "Data Analysis with Python",
+        projects: [{
+                prolink: "https://juvenileflawlessentropy.krishnasai4.repl.run/",
+                paragraph: "M/V/STD calculator",
+            },
+            {
+                prolink: "https://flatemotionalnumber.krishnasai4.repl.run/",
+                paragraph: "Demographic Data analysis",
+            },
+            {
+                prolink: "https://giantcoldtab.krishnasai4.repl.run/",
+                paragraph: "Medical Data Visualizer",
+            },
+            {
+                prolink: "https://activebelatedrate.krishnasai4.repl.run/",
+                paragraph: "Page View Time Series",
+            },
+            {
+                prolink: "https://informalbigoolanguage.krishnasai4.repl.run/",
+                paragraph: "Sea Level Predictor",
+            },
+        ],
+    },
+    {
+        technology: "Information Security",
+        projects: [{
+                prolink: "https://agreeable-busy-volleyball.glitch.me/",
+                paragraph: "Stock Price Checker",
+            },
+            {
+                prolink: "https://fluff-wooded-calculator.glitch.me/",
+                paragraph: "Anonymous Message Board",
+            },
+            {
+                prolink: "https://fcc-port-scanner.krishnasai4.repl.run/",
+                paragraph: "Port Scanner",
+            },
+            {
+                prolink: "https://fcc-brute-force-password-cracker.krishnasai4.repl.run/",
+                paragraph: "SHA-1 password",
+            },
+            {
+                prolink: "https://boilerplate-project-secure-real-time-multiplayer-game.krishnasai4.repl.co/",
+                paragraph: "Real Time Multiplayer",
+            },
+        ],
+    },
+    {
+        technology: "Machine Learning with Python",
+        projects: [{
+                prolink: "https://repl.it/repls/KindheartedMonumentalScientist",
+                paragraph: "Rock Paper Scissors",
+            },
+            {
+                prolink: "https://colab.research.google.com/drive/112bLjBaxkKnLqZJdH51ca8frNnBK0JWm?usp=sharing",
+                paragraph: "Cat and Dog Image Classifier",
+            },
+            {
+                prolink: "https://colab.research.google.com/drive/1pJKE0Rkm1n0togybbFuNc7MAlywD-Oee?usp=sharing",
+                paragraph: "Book recommendation",
+            },
+            {
+                prolink: "https://colab.research.google.com/drive/1mSu9Itt7ICIBrwipcvIsEECT7vrH1mKF?usp=sharing",
+                paragraph: "Health Costs Calculator",
+            },
+            {
+                prolink: "https://colab.research.google.com/drive/1FAkph2u2EM9n9rj9PjBQvQDa_t3IGfJX?usp=sharing",
+                paragraph: "SMS Text Classifier",
+            },
+        ],
+    },
+];
--- script.js
+++ script.js
@@ -1,69 +1,73 @@
-var a = document.getElementById("projects-grid");
-/*
-                                                  {
-                                                    prolink: '',
-                                                    imglink: '',
-                                                    paragraph: ''
-                                                  },
-                                                  */
-var list = [{
-        prolink: "https://codepen.io/tznqeyiq/full/wvMxPOb",
-        imglink: "https://media2.picsearch.com/is?yR67uiGC1QoGer1ndxdS1qD5en5C3Vyyc4FZUKywEzQ&height=250",
-        paragraph: "TRIBUTE PAGE",
-    },
-
-    {
-        prolink: "https://codepen.io/tznqeyiq/full/wvMxpjM",
-        imglink: "./images/Form.jpg",
-        paragraph: "FORM",
-    },
-
-    {
-        prolink: "https://codepen.io/tznqeyiq/full/XWXBEVd",
-        imglink: "https://media1.picsearch.com/is?yruPQEMk3koPa7J-Ii3j0HFNItjUktTrM8TB8YmN6dk&height=256",
-        paragraph: "PRODUCT LANDING",
-    },
-
-    {
-        prolink: "https://rogue-plume-bug.glitch.me/",
-        imglink: "./images/mertic-imp.jpg",
-        paragraph: "Metric/Imp Converter",
-    },
-
-    {
-        prolink: "https://rhetorical-hurricane-rugby.glitch.me/",
-        imglink: "./images/IssueTracker.jpg",
-        paragraph: "Issue Tracker",
-    },
-    {
-        prolink: "https://hilarious-aromatic-sovereign.glitch.me/",
-        imglink: "./images/PersonalLibrary.jpg",
-        paragraph: "Personal Library",
-    },
+let projects_grid = document.getElementById("projects-grid");
+const all_projects = [
+    "Front End Libraries",
+    "Data Visualization",
+    "APIs and Microservices",
+    "Quality Assurance",
+    "Scientific Computing with Python",
+    "Data Analysis with Python",
+    "Information Security",
+    "Machine Learning with Python",
+    "Responsive Web Design",
 ];
-for (var i in list) {
-    var newproject = document.createElement("div");
-    newproject.setAttribute("class", "project");
-    var anchor = document.createElement("a");
-    anchor.setAttribute("class", `project-tile ${i}`);
-    anchor.setAttribute("target", "_blank");
-    anchor.href = list[i].prolink;
-    anchor.innerHTML = `<img src="${list[i].imglink}"/><p class="project-title"> ${list[i].paragraph} </p>`;
-    newproject.appendChild(anchor);
-    a.appendChild(newproject);
-}
-var modal = document.getElementById("modal-wrapper");
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+let project_types = {
+    Html_CSS_JS: ["Responsive Web Design"],
+    React_JS: ["Front End Libraries"],
+    Data_Analysis: ["Data Visualization", "Data Analysis with Python"],
+    Python_DA_ML: [
+        "Data Analysis with Python",
+        "Machine Learning with Python",
+        "Scientific Computing with Python",
+        "Information Security",
+    ],
+    Node_JS: ["Quality Assurance", "APIs and Microservices"],
 };
-var smallbutton = document.getElementById("smallbutton");
+// The List is coming from ./projects.js file
+let htmlResult = "";
+for (let i = 0; i < list.length; i++) {
+    let { technology } = list[i];
+    htmlResult += `
+    <div class="project-type" id="${technology}">
+    <h1>${technology}</h1>
+    `;
+    for (let j = 0; j < list[i]["projects"].length; j++) {
+        htmlResult += `
+      <div class="project">
+      <a href="${list[i]["projects"][j]["prolink"]}" target="_blank">
+        ${list[i]["projects"][j]["paragraph"]}
+      </a>
+      </div>
+      `;
+    }
+    htmlResult += `
+    </div>`;
+}
+projects_grid.innerHTML = htmlResult;
 
-function createproject(form) {
-    var newobject = {
-        prolink: document.getElementById("projlink").value,
-        imglink: document.getElementById("projimg").value,
-        paragraph: document.getElementById("projname").value,
-    };
+all_projects.forEach(
+    (project) => (document.getElementById(project).style.display = "none")
+);
+project_types["Html_CSS_JS"].forEach(
+    (project) => (document.getElementById(project).style.display = "inline-block")
+);
+
+function changeProjects(target) {
+    let { value } = target;
+    all_projects.forEach(
+        (project) => (document.getElementById(project).style.display = "none")
+    );
+    project_types[value].forEach(
+        (project) =>
+        (document.getElementById(project).style.display = "inline-block")
+    );
+}
+
+function submitHandler() {
+    let name = document.getElementById("name");
+    let body = document.getElementById("body");
+    let value = `
+  name:${name.value}
+  ${body.value}`;
+    window.location.href =
+        "mailto:chinnacks@outlook.com?subject=Portfolio Feedback&body=" + value;
 }
